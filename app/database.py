d0756a49cab843c98b94d48d77c56535ad19a4c2
LAPTOP-FAVNB76U\shequ <shequet@gmail.com>
2020-10-01 09:06:00 +0200

    Refacto database

--- app/database.py
+++ app/database.py
@@ -9,11 +9,6 @@ class Database:
     """ Database class"""
 
     def __init__(self):
-        self.connect()
-
-    def connect(self):
-        """ Connection in database"""
-
         self.conn = mysql.connector.connect(
             host=os.environ.get('DATABASE_HOST', '127.0.0.1'),
             user=os.environ.get('DATABASE_USER', 'root'),
@@ -21,29 +16,22 @@ class Database:
             password='123456',
             database=os.environ.get('DATABASE_NAME', 'openfoodfact')
         )
+        self.conn.autocommit = True
+        self.cursor = self.conn.cursor()
 
     def execute(self, query, params=()):
         """ Execute query in database"""
 
-        cursor = self.conn.cursor()
-        cursor.execute(query, params)
-        self.conn.commit()
-        cursor.close()
+        self.cursor.execute(query, params)
 
     def fetch_one(self, query, params=()):
         """ Fetch one record """
 
-        cursor = self.conn.cursor()
-        cursor.execute(query, params)
-        row = cursor.fetchone()
-        cursor.close()
-        return row
+        self.execute(query, params)
+        return self.cursor.fetchone()
 
     def fetch_all(self, query, params=()):
         """ Fetch all record """
 
-        cursor = self.conn.cursor()
-        cursor.execute(query, params)
-        rows = cursor.fetchall()
-        cursor.close()
-        return rows
+        self.execute(query, params)
+        return self.cursor.fetchall()
